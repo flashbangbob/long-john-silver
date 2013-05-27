62de7703fdeb33f48d9f72689ac1a00cb9b5fb1e
Bob <flashbangbob@gmail.com>
2013-05-27 19:13:38 +0000

    Fixes

--- api/dbconn.py
+++ api/dbconn.py
@@ -1,7 +1,8 @@
-import MySQLdb as mdb
+import MySQLdb
+import MySQLdb.cursors
 
 def get_new_connection():
-	return mdb.connect(host = 'localhost', user = 'flashbangbob', passwd = '5022', db = 'main', cursorclass = mdb.cursors.DictCursor)
+	return MySQLdb.connect(host = 'localhost', user = 'flashbangbob', passwd = '5022', db = 'main', cursorclass = MySQLdb.cursors.DictCursor)
 		
 		
 		
