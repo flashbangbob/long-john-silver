3b64d10305ac6f0bb6d486b63a72263124305f7e
Bob <flashbangbob@gmail.com>
2013-05-27 09:22:26 -0500

    Adding ShopTypeAPI

--- api/main.py
+++ api/main.py
@@ -1,21 +1,34 @@
 from flask import Flask, jsonify
 import user_api
 import corp_api
+import shop_type_api
 import MySQLdb as mdb
 import json
 
 app = Flask(__name__)
 app.config.update(DEBUG=True)
 
+'''
+RTE - ROOT
+'''        
+
 @app.route('/')
 def hello_world():
         data = {"id": 5, "title": "Myworld"}
         return jsonify(**data)
-        
+
+'''
+RTE - CORP
+'''        
+
 @app.route('/corp/<int:id>')
 def get_corp_for_user_id(id):
 	corp = corp_api.get_corp_for_user_id(id)
 	return jsonify(corp=corp)
+
+'''
+RTE - USERS
+'''
 
 @app.route('/user/')
 def get_users():
@@ -32,5 +45,31 @@ def get_users_by_name(username):
 	row = user_api.get_users_by_name(username)
     	return jsonify(users = row)
 
+'''
+RTE - SHOPTYPE
+'''
+
+@app.route('/user/')
+def get_shop_types():
+	rows = shop_type_api.get_shop_types()
+	return jsonify(shop_types = rows)
+
+
+'''
+PROGRAMRUN
+'''
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10080)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ api/shop_type_api.py
@@ -0,0 +1,10 @@
+import MySQLdb as mdb
+
+con = mdb.connect('localhost', 'flashbangbob', '5022', 'main')
+
+def get_shop_types():
+	with con:
+		cur = con.cursor(mdb.cursors.DictCursor)
+		cur.execute("SELECT * FROM shop_type", id)
+		rows = cur.fetchone()
+		return rows
