bc49172f38ddea4f63d7861742fbdbf55d8ec134
Bob <flashbangbob@gmail.com>
2013-05-26 22:04:29 -0500

    Fixes

--- api/main.py
+++ api/main.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify
+import user_api
+import corp_api
 import MySQLdb as mdb
 import json
 
 app = Flask(__name__)
 app.config.update(DEBUG=True)
-
-con = mdb.connect('localhost', 'flashbangbob', '5022', 'main')
 
 @app.route('/')
 def hello_world():
@@ -14,45 +14,23 @@ def hello_world():
         
 @app.route('/corp/<int:id>')
 def get_corp_for_user_id(id):
-	with con:
-		cur = con.cursor(mdb.cursors.DictCursor)
-		cur.execute("SELECT id, name, ticker, money, share_price FROM corporation WHERE id = %s", id)
-		rows = cur.fetchall()
-		return jsonify(corp = rows)
-		
-def get_corp_for_user_id_json(id):
-	with con:
-		cur = con.cursor(mdb.cursors.DictCursor)
-		cur.execute("SELECT id, name, ticker, money, share_price FROM corporation WHERE id = %s", id)
-		rows = cur.fetchone()
-		return rows
+	corp = corp_api.get_corp_for_user_id(id)
+	return jsonify(corp=corp)
 
 @app.route('/user/')
 def get_users():
-	with con:
-		cur = con.cursor(mdb.cursors.DictCursor)
-		cur.execute("SELECT id, username, email FROM user")
-		rows = cur.fetchall()
-		return jsonify(users = rows)
+	rows = user_api.get_users()
+	return jsonify(users = rows)
 
 @app.route('/user/<int:id>')
 def get_users_by_id(id):
-        with con:
-                cur = con.cursor(mdb.cursors.DictCursor)
-                cur.execute("SELECT id, username, email FROM user WHERE id = %s", id)
-                row = cur.fetchone()
-                row['corp'] = get_corp_for_user_id_json(row['id'])
-                return jsonify(users = row)
+	row = user_api.get_users_by_id(id)
+    return jsonify(users = row)
 
 @app.route('/user/<string:username>')
 def get_users_by_name(username):
-        with con:
-                cur = con.cursor(mdb.cursors.DictCursor)
-                cur.execute("SELECT id, username, email FROM user WHERE username = %s", username)
-                row = cur.fetchone()
-                row['corp'] = get_corp_for_user_id_json(row['id'])
-                return jsonify(users = row)
-
+	row = user_api.get_users_by_username(username)
+    return jsonify(users = row)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10080)
--- api/user_api.py
+++ api/user_api.py
@@ -1,34 +1,27 @@
-from flask import Blueprint, jsonify
 from corp_api import corp_api
 import MySQLdb as mdb
-import sys
-import json
 
 con = mdb.connect('localhost', 'flashbangbob', '5022', 'main')
-user_api = Blueprint('user_api', __name__)
 
-@user_api.route('/user/')
 def get_users():
 	with con:
 		cur = con.cursor(mdb.cursors.DictCursor)
 		cur.execute("SELECT id, username, email FROM user")
 		rows = cur.fetchall()
-		return jsonify(users = rows)
+		return rows
 
-@user_api.route('/user/<int:id>')
 def get_users_by_id(id):
         with con:
                 cur = con.cursor(mdb.cursors.DictCursor)
                 cur.execute("SELECT id, username, email FROM user WHERE id = %s", id)
                 row = cur.fetchone()
                 row['corp'] = corp_api.get_corp_for_user_id(row['id'])
-                return jsonify(users = row)
+        		return row
 
-@user_api.route('/user/<string:username>')
 def get_users_by_name(username):
         with con:
                 cur = con.cursor(mdb.cursors.DictCursor)
                 cur.execute("SELECT id, username, email FROM user WHERE username = %s", username)
                 row = cur.fetchone()
                 row['corp'] = corp_api.get_corp_for_user_id(row['id'])
-                return jsonify(users = row)
+                return row;
