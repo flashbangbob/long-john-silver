36514f743b96449080432e78ee7ab7913a5712cf
flashbangbob <flashbangbob@gmail.com>
2013-06-01 13:48:25 -0500

    Fixes

--- server/stockmover.py
+++ server/stockmover.py
@@ -1,7 +1,10 @@
 import dbconn
+import time
 
 #UPDATE STOCKS
 con = dbconn.get_new_connection()
-with con:
-    cur = con.cursor()
-    cur.execute("UPDATE corporation SET share_price = share_price * (1 + ((RAND() - .5) / 1000))")
+for x in range(0,6)
+    with con:
+        cur = con.cursor()
+        cur.execute("UPDATE corporation SET share_price = share_price * (1 + ((RAND() - .5) / 1000))")
+    time.sleep(10)
