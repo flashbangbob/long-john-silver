26ae8a726f28e4791ac478b4d9b1e62ccc636d88
flashbangbob <flashbangbob@gmail.com>
2013-06-05 17:06:06 -0500

    Fixes

--- server/npc_results.py
+++ server/npc_results.py
@@ -2,6 +2,6 @@ import dbconn
 
 #UPDATE STOCKS
 con = dbconn.get_new_connection()
-    with con:
-        cur = con.cursor()
-        cur.execute("UPDATE corporation SET share_price = share_price * (1 + ((RAND() - .5) / 10)) where is_npc = 1")
+with con:
+    cur = con.cursor()
+    cur.execute("UPDATE corporation SET share_price = share_price * (1 + ((RAND() - .5) / 10)) where is_npc = 1")
