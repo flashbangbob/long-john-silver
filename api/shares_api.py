92163c5d71369ede534d9b7fcf104219280348e0
Bob <flashbangbob@gmail.com>
2013-06-05 02:11:59 +0000

    Fixes

--- api/shares_api.py
+++ api/shares_api.py
@@ -5,6 +5,6 @@ def get_shares_for_corp_id(id):
 	con = dbconn.get_new_connection()
 	with con:
 		cur = con.cursor()
-		cur.execute("SELECT corporation_id, count(*) as quantity, FROM shares WHERE owning_corporation_id = %s GROUP BY corporation_id", id)
+		cur.execute("SELECT corporation_id, count(*) as quantity FROM shares WHERE owning_corporation_id = %s GROUP BY corporation_id", id)
 		rows = cur.fetchall()
 		return rows
